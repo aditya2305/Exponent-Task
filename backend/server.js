edf91657aa059ea463fbb0a1c57f28d9b1e83e76
aditya2305 <aditya.saxena33@gmail.com>
2025-01-24 03:26:36 +0530

    backend abstraction

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import cors from 'cors';
 import getUserData from './jobs/getUserData.js';
 import incrementUserScore from './jobs/incrementUserScore.js';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://adityasaxena33:Yz6gM92eWUWbgv5w@cluster0.lhm8x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
     console.log('Database connection successful!');
+})
+.catch((err) => {
+    console.error('Database connection error:', err);
 });
+
 
 app.get('/api/user/:id', getUserData);
 app.post('/api/user/:id/click', incrementUserScore);
@@ -19,4 +26,7 @@ app.get("/", (req, res)=>{
     res.send("started")
 })
 
-app.listen(3001, '0.0.0.0', ()=>{"Server started on port 3001"});
+const PORT = process.env.PORT || 3001;
+const HOST = process.env.HOST || '0.0.0.0';
+
+app.listen(PORT, HOST, ()=>{"Server started on port 3001"});
